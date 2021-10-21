d4e22ba4bded74222e8bbf3d91959a8154e4a9dc
juliapalorinne <julia.palorinne@helsinki.fi>
2021-10-21 20:08:15 +0300

    tehtävä 2.1

---
+++ osa2/kurssisovellus/src/components/Course.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <h1>{props.title}</h1>
+  )
+}
+
+const Content = (props) => {
+  const list = props.parts.map(p => <Part part={p}/>)
+  return (list)
+}
+
+const Part = (props) => {
+  const line = <p>{props.part.name} {props.part.exercises}</p>
+  return (line)
+}
+
+const Total = (props) => {
+  let total = 0
+  props.parts.forEach(part => {
+    total += part.exercises
+  })
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+const Course = ({ course }) => { 
+  return (
+    <div>
+      <Header title={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
+  )
+}
+
+export default Course
